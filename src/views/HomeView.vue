0a4ff30a97d7240d5dc0cf53b46c0b43dbe64d32
eric <howcrazys@gmail.com>
2024-02-01 21:20:31 +0800

    feat: 'init'

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+
+
+const a = 2123;
+console.log(a);
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
